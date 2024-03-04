c1da200713c8aad295869e68e6c8e4ee95d9c0c9
HuyDev <huytran20035@gmail.com>
2024-03-05 00:01:41 +0700

    updated

--- client/src/components/Drawer/index.jsx
+++ client/src/components/Drawer/index.jsx
@@ -1,46 +1,50 @@
-import PropTypes from "prop-types"
-import "./drawer.module.css"
-import logo from "../../assets/images/logo/favicon_48.png"
+import PropTypes from 'prop-types';
+import styles from './drawer.module.css';
+import logo from '../../assets/images/logo/favicon_48.png';
+import { NavLink } from 'react-router-dom';
 
 const Drawer = () => {
-    return (
-        <div id="drawer" className="fixed top-0 bottom-0 bg-[#0f0f0f] touch-pan-y h-100">
-            <div className="drawer__content top-0 left-0 w-[240px]">
-                <div className="drawer__content-header p-3">
-                    <a href="/" className="flex header-logo items-center h-[50px] cursor-pointer">
-                        <i className="ri-menu-line opacity-50 hover:opacity-100 text-white mr-3 ri-xl cursor-pointer p-3 block hover:before:content-['\ef32']"></i>
-                        <img src={logo} alt="" className="h-[28px] w-[28px]" />
-                        <h3 className="font-[Oswald] ml-1 text-2xl font-bold text-white">Music</h3>
-                    </a>
-                </div>
-                <div className="drawer__content-body">
-                    <div className="naivgation">
-                        <div className="text-white p-2">
+  return (
+    <div id="drawer" className="fixed top-0 bottom-0 bg-[#0f0f0f] touch-pan-y h-100">
+      <div className=" top-0 left-0 w-[240px]">
+        <div className="p-3">
+          <div className="flex items-center h-[50px] cursor-pointer">
+            <i className="ri-menu-line opacity-50 hover:opacity-100 text-white mr-3 ri-xl cursor-pointer p-3 block hover:before:content-['\ef32']"></i>
+            <a href="/" className="flex items-center">
+              <img src={logo} alt="" className="h-[26px] w-[26px]" />
+              <h3 className="font-[Oswald] ml-1 text-2xl font-bold text-white">Music</h3>
+            </a>
+          </div>
 
-                        </div>
-                        <NavLink text="Home" href="/"/> 
-                    </div>
-
-                </div>
-                </div>
+          <ul className="navigation p-2">
+            <li className="text-white w-100">
+              <NavLink to="/home" exact className="inline-block w-[100%]">
+                Home
+              </NavLink>
+            </li>
+          </ul>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-const NavLink = ({text, href}) => {
-    return (
-        <>
-                    
-                            <div className="nav__link p-4 bg-neutral-800 cursor-pointer rounded-lg">
-                                <a href={href}>{text}</a>
-                            </div>
-        </>
-    )
-}
+// const NavLink = ({ text, href, className, children }) => {
+//   return (
+//     <>
+//       <div className={`p-4 mt-2 bg-neutral-800 cursor-pointer rounded-lg text-sm ${className} group ${({isActive}) => {isActive ? activeLink: ''}}`}>
+//         {children}
+//         <a className={({ isActive }) => (isActive ? activeLink : '')} href={href}>
+//           {text}
+//         </a>
+//       </div>
+//     </>
+//   );
+// };
 
-NavLink.propTypes = {
-    text: PropTypes.string,
-    href: PropTypes.string
-}
+// NavLink.propTypes = {
+//   text: PropTypes.string,
+//   href: PropTypes.string,
+// };
 
-export {Drawer}
+export { Drawer };
